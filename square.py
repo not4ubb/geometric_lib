23d55014000ee4caed216a3775bfeac641846c0d
Ivan Londar <londarivan32@gmail.com>
2024-11-25 20:02:00 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,7 +1,10 @@
-
 def area(a):
+    if a < 0:
+        raise ValueError("Side length cannot be negative")
     return a * a
 
 
 def perimeter(a):
+    if a < 0:
+        raise ValueError("Side length cannot be negative")
     return 4 * a
