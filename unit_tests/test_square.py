48aade42825fb071d98ee41eb9f91b5d3b3d8aa1
Ivan Londar <londarivan32@gmail.com>
2024-11-25 20:03:05 +0300

    Update test_square.py

--- unit_tests/test_square.py
+++ unit_tests/test_square.py
@@ -1,20 +1,36 @@
 import unittest
+import sys
 from square import area, perimeter
 
-class TestSquare(unittest.TestCase):
-    def test_area(self):
-        self.assertEqual(area(4), 16)
-        self.assertEqual(area(0), 0)
+sys.path.append("..")
 
-    def test_perimeter(self):
-        self.assertEqual(perimeter(4), 16)
-        self.assertEqual(perimeter(0), 0)
+
+class SquareTestCase(unittest.TestCase):
+    def test_zero_side(self):
+        side = 0
+        expected_area = 0
+        expected_perimeter = 0
+
+        self.assertEqual(area(side), expected_area)
+        self.assertEqual(perimeter(side), expected_perimeter)
+
+    def test_positive_side(self):
+        side = 1
+        expected_area = 1
+        expected_perimeter = 4
+
+        self.assertEqual(area(side), expected_area)
+        self.assertEqual(perimeter(side), expected_perimeter)
 
     def test_negative_side(self):
+        side = -1
+
         with self.assertRaises(ValueError):
-            area(-4)
+            area(side)
+
         with self.assertRaises(ValueError):
-            perimeter(-4)
+            perimeter(side)
+
 
 if __name__ == "__main__":
     unittest.main()
