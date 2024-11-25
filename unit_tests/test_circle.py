eada8aa994897c424eb5b9d966890aff2a27908a
Ivan Londar <londarivan32@gmail.com>
2024-11-25 20:02:38 +0300

    Update test_circle.py

--- unit_tests/test_circle.py
+++ unit_tests/test_circle.py
@@ -1,20 +1,46 @@
 import unittest
+import math
+import sys
 from circle import area, perimeter
 
-class TestCircle(unittest.TestCase):
-    def test_area(self):
-        self.assertAlmostEqual(area(3), 28.274333882308138)
-        self.assertAlmostEqual(area(0), 0)
+sys.path.append("..")
 
-    def test_perimeter(self):
-        self.assertAlmostEqual(perimeter(3), 18.84955592153876)
-        self.assertAlmostEqual(perimeter(0), 0)
+
+class CircleTestCase(unittest.TestCase):
+    def test_zero_radius(self):
+        radius = 0
+        expected_area = 0
+        expected_perimeter = 0
+
+        self.assertEqual(area(radius), expected_area)
+        self.assertEqual(perimeter(radius), expected_perimeter)
+
+    def test_positive_radius(self):
+        radius = 1
+        expected_area = math.pi
+        expected_perimeter = 2 * math.pi
+
+        self.assertEqual(area(radius), expected_area)
+        self.assertEqual(perimeter(radius), expected_perimeter)
 
     def test_negative_radius(self):
+        radius = -1
+
         with self.assertRaises(ValueError):
-            area(-3)
+            area(radius)
+
         with self.assertRaises(ValueError):
-            perimeter(-3)
+            perimeter(radius)
+
+    def test_non_numeric_radius(self):
+        radius = "string"
+
+        with self.assertRaises(TypeError):
+            area(radius)
+
+        with self.assertRaises(TypeError):
+            perimeter(radius)
+
 
 if __name__ == "__main__":
     unittest.main()
