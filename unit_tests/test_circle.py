06be825248396287d2c62b71b6d48636fe3e6ce8
Ivan Londar <londarivan32@gmail.com>
2024-11-26 23:02:27 +0300

    Update test_circle.py

--- unit_tests/test_circle.py
+++ unit_tests/test_circle.py
@@ -6,38 +6,46 @@ from circle import area, perimeter
 sys.path.append("..")
 
 
-class CircleTestCase(unittest.TestCase):
+class CircleAreaTestCase(unittest.TestCase):
     def test_zero_radius(self):
         radius = 0
         expected_area = 0
-        expected_perimeter = 0
-
         self.assertEqual(area(radius), expected_area)
-        self.assertEqual(perimeter(radius), expected_perimeter)
 
     def test_positive_radius(self):
         radius = 1
         expected_area = math.pi
-        expected_perimeter = 2 * math.pi
-
         self.assertEqual(area(radius), expected_area)
+
+    def test_negative_radius(self):
+        radius = -1
+        with self.assertRaises(ValueError):
+            area(radius)
+
+    def test_non_numeric_radius(self):
+        radius = "string"
+        with self.assertRaises(TypeError):
+            area(radius)
+
+
+class CirclePerimeterTestCase(unittest.TestCase):
+    def test_zero_radius(self):
+        radius = 0
+        expected_perimeter = 0
+        self.assertEqual(perimeter(radius), expected_perimeter)
+
+    def test_positive_radius(self):
+        radius = 1
+        expected_perimeter = 2 * math.pi
         self.assertEqual(perimeter(radius), expected_perimeter)
 
     def test_negative_radius(self):
         radius = -1
-
-        with self.assertRaises(ValueError):
-            area(radius)
-
         with self.assertRaises(ValueError):
             perimeter(radius)
 
     def test_non_numeric_radius(self):
         radius = "string"
-
-        with self.assertRaises(TypeError):
-            area(radius)
-
         with self.assertRaises(TypeError):
             perimeter(radius)
 
